0f54be12704643406a9ccba3b0f17b8d29755976
Akshaya2019 <akshaya122019@gmail.com>
2025-03-17 19:05:26 +0530

    event

--- js/asynchronous.js
+++ js/asynchronous.js
@@ -1,5 +1,15 @@
 //Asynchronous JS
+
+
+//Synchronous Method
 console.log("First");
 console.log("Second");
 console.log("Third");
 
+//Asynchronous Method
+console.log("First");
+//set timeout for 2 sec
+setTimeout(()=> console.log("Second"),2000);
+console.log("Third")
+
+//callback
